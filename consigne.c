3ad2da8415556874faaa4ea76ad71be24a6d002a
Colin <colinrousseau35@gmail.com>
2026-03-03 14:47:37 +0100

    les 2 visualisations + consigne

--- consigne.c
+++ consigne.c
@@ -4,8 +4,22 @@
    {
    
       float thermostat_f=0;
-   
-     
-      return thermostat_f;
-   
-   }
+      FILE *fptr;
+
+      // Open a file in read mode
+      fptr = fopen(".verrouConsigne", "r");
+      if (fptr != NULL) {
+         printf("Le fichier .verrou existe\n");
+         fclose(fptr);
+         return thermostatPrec_f;
+      }
+      else {
+         printf("Le fichier .verrou n'existe pas\n");
+         fptr = fopen("consigne.txt", "r");
+         if (fptr != NULL) {
+            fscanf(fptr, "%f", &thermostat_f);
+            fclose(fptr);
+        }
+        return thermostat_f;
+    }
+}
--- visualisationC.c
+++ visualisationC.c
@@ -6,6 +6,34 @@
 
    void visualisationC(float puissance_f)
    {
-            	
-   
+      FILE *fptr;
+      float ligne2 = 0;
+      float ligne3 = 0;
+      char temoin[10];
+      char ligne1[10];
+      fptr = fopen(".verrouData", "r");
+      if (fptr != NULL) {
+         printf("Le fichier .verrouData existe\n");
+         fclose(fptr);
+         return;
+      }
+      else {
+         printf("Le fichier .verrouData n'existe pas\n");
+         fptr = fopen("data.txt", "r");
+         if (fptr != NULL) {
+            fscanf(fptr, "%s", ligne1); 
+            fscanf(fptr, "%f", &ligne2);
+            fscanf(fptr, "%f", &ligne3);
+            fclose(fptr);
+
+            if (puissance_f > 0) strcpy(temoin, "true");
+            else strcpy(temoin, "false");
+
+            fptr = fopen("data.txt", "w");
+            fprintf(fptr, "%s\n", temoin);
+            fprintf(fptr, "%.2f\n", ligne2); 
+            fprintf(fptr, "%.2f\n", ligne3);
+            fclose(fptr);
+         }
+      }
    }
